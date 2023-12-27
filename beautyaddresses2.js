fa57671722580cd0221be8562dc4a108c9011589
DrDro7 <81784870+DrDro7@users.noreply.github.com>
2023-12-26 22:17:02 -0500

    Create beautyaddresses2.js

---
+++ beautyaddresses2.js
@@ -0,0 +1,328 @@
+const beauty_locations = {
+
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -80.28167931741967,
+          25.70918514884478
+
+        ]
+      },
+      "properties": {
+        "address": "1430 Madruga Ave",
+        "city": "Coral Gables",
+        "country": "United States",
+        "crossStreet": "at Santona St",
+        "postalCode": "33146",
+        "state": "FL",
+        "text": "My Suite!@BeautySquare Coral Gables",
+      }
+    },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.16667625571557,
+    25.9458335466 ]
+   },
+   "properties": {
+
+
+     "address": "1660 NE Miami Gardens Dr Suites 6",
+     "city": "Aventura",
+     "country": "United States",
+     "postalCode": "33179",
+     "state": "FL",
+     "text": "Aventura Beauty Mall",
+   
+    }
+   },
+
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.35355131793,
+    25.599551034844268 ]
+   },
+   "properties": {
+
+
+     "address": "18423 South Dixie Hwy",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33157",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Cutler Bay",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.36774753571895,
+    25.813014352425313 ]
+   },
+   "properties": {
+
+
+     "address": "10555 NW 41 ST",
+     "city": "Doral",
+     "country": "United States",
+     "postalCode": "33178",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Doral",
+     
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.31867775603153,
+    25.85481636001674 ]
+   },
+   "properties": {
+
+
+     "address": "3600 W 18 Ave",
+     "city": "Hialeah",
+     "country": "United States",
+     "postalCode": "33012",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Hialeah",
+
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.3850449569823,
+    25.66393821263597 ]
+   },
+   "properties": {
+
+
+     "address": "13550 SW 120th St Suite 506",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33186",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Kendall",
+   
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.19914000379045,
+    26.272338164804438 ]
+   },
+   "properties": {
+
+
+     "address": "5400 West Sample Rd",
+     "city": "Margate",
+     "country": "United States",
+     "postalCode": "33073",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Margate",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.19457762729368,
+    25.80699567796256 ]
+   },
+   "properties": {
+
+
+     "address": "3201 N Miami Ave",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33127",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Midtown",
+ 
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.30890297957015,
+    25.976977706858193 ]
+   },
+   "properties": {
+
+
+     "address": "12204 Miramar Parkway",
+     "city": "Miramar",
+     "country": "United States",
+     "postalCode": "33025",
+     "state": "FL",
+     "text": "Miramar Beauty Mall",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.26104075510719,
+    26.149020242201573 ]
+   },
+   "properties": {
+
+
+     "address": "8249 West Sunrise Blvd.",
+     "city": "Plantation",
+     "country": "United States",
+     "postalCode": "33322",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Plantation",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.30574330970009,
+    25.733213678933396 ]
+   },
+   "properties": {
+
+
+     "address": "6840 SW 40 St Suite 212",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33155",
+     "state": "FL",
+     "text": "South Miami Beauty Mall",
+
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.25226761685562,
+    26.208470047459585 ]
+   },
+   "properties": {
+
+
+     "address": "6738 N. University Drive",
+     "city": "Tamarac",
+     "country": "United States",
+     "postalCode": "33321",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Tamarac",
+     
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.35116725587811,
+    25.769181165308936 ]
+   },
+   "properties": {
+
+
+     "address": "9509 West Flagler St",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33174",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare West Flagler",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.3033844,
+    26.1091693 ]
+   },
+   "properties": {
+
+
+     "address": "11336 W. State Road 84",
+     "city": "Davie",
+     "country": "United States",
+     "postalCode": "33325",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Davie",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.340426,
+    25.8247917 ]
+   },
+   "properties": {
+
+
+     "address": "5645 NW 87th Ave",
+     "city": "Doral",
+     "country": "United States",
+     "postalCode": "33178",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare Doral 87",
+    
+    }
+   },
+    {
+     "type": "Feature",
+     "geometry": {
+     "type": "Point",
+     "coordinates": [
+    -80.43814931225658,
+    25.698787440389495 ]
+   },
+   "properties": {
+
+
+     "address": "15140 Sw 72 St",
+     "city": "Miami",
+     "country": "United States",
+     "postalCode": "33193",
+     "state": "FL",
+     "text": "My Suite!@BeautySquare West Kendall",
+     
+    }
+   }
+    
+    ]
+};
